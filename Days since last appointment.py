0925bd07bcf0ee937d6f8428e61fef866a265170
Chris Howitt <62956122+chrishowitt123@users.noreply.github.com>
2022-05-18 14:49:39 +0100

    Create Days since last appointment.py

---
+++ Days since last appointment.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import os
+import datetime as dt
+import numpy as np
+os.chdir(r'M:\MSG Open Episodes\Apointments pull 24.01.22')
+
+df = pd.read_csv('apptsFull.txt', '\t')
+
+# df = df.head(1000)
+df.columns=['URN', 'EpisodeNumber', 'CareProvider', 'Speciality', 'Unknown', 'AppointmentDate', 'AppointmentTime', 'AptCareProvider', 'AptSpeciality', 'Group']
+df['AppointmentDate'] =  pd.to_datetime(df['AppointmentDate'],errors='coerce')
+
+df = df.sort_values(['URN','EpisodeNumber','AppointmentDate', 'AppointmentTime'],ascending=[True,False,False,False])
+cols = list(df.columns)
+df
+
+ep_groups = df.groupby('EpisodeNumber')
+
+results_list = []
+
+for k,v in ep_groups:
+    v['PreviousApt'] = v['AppointmentDate'].shift(-1)
+    v['DaysSincePrivousAppiontment'] = v['AppointmentDate'] -  v['PreviousApt']
+    df_out = v
+    results_list.append(df_out)
+    
+df_res =  pd.concat(results_list)
+
+df_res['DaysSincePrivousAppiontment'] = df_res['DaysSincePrivousAppiontment'].dt.total_seconds()
+df_res['DaysSincePrivousAppiontment'] = df_res['DaysSincePrivousAppiontment'] / 60 / 60 / 24
+
+years = 10
+number_days = years*365
+
+df_res = df_res[df_res['DaysSincePrivousAppiontment'] > number_days]
+
+df_res.to_excel('Days between appointments.xlsx', index= False)
+df_res
