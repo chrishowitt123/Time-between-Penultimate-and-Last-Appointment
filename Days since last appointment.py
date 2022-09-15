0b1948194f29b79752e34a78c3059fb75ed7cb76
Chris Howitt <62956122+chrishowitt123@users.noreply.github.com>
2022-09-15 20:19:26 +0100

    Update Days since last appointment.py

--- Days since last appointment.py
+++ Days since last appointment.py
@@ -12,33 +12,42 @@ A program that returns the number of days between the penultimate and last appoi
 # import all appointments
 df = pd.read_csv('apptsFull.txt', '\t')
 
-# df = df.head(1000)
+# define column headers 
 df.columns=['URN', 'EpisodeNumber', 'CareProvider', 'Speciality', 'Unknown', 'AppointmentDate', 'AppointmentTime', 'AptCareProvider', 'AptSpeciality', 'Group']
+
+# change column headers
+cols = list(df.columns)
+
+# ensure that all dates to be processed are in DT format
 df['AppointmentDate'] =  pd.to_datetime(df['AppointmentDate'],errors='coerce')
 
+# sort values
 df = df.sort_values(['URN','EpisodeNumber','AppointmentDate', 'AppointmentTime'],ascending=[True,False,False,False])
-cols = list(df.columns)
-df
 
+# group data into episodes of care
 ep_groups = df.groupby('EpisodeNumber')
 
 results_list = []
-
 for k,v in ep_groups:
+    # define penultimate appointment date within episode of care
     v['PreviousApt'] = v['AppointmentDate'].shift(-1)
+    # calculate date difference
     v['DaysSincePrivousAppiontment'] = v['AppointmentDate'] -  v['PreviousApt']
+    # append group (episode of care) to results list
     df_out = v
     results_list.append(df_out)
-    
+
+# results list to DataFrame    
 df_res =  pd.concat(results_list)
 
+# convery into standard units
 df_res['DaysSincePrivousAppiontment'] = df_res['DaysSincePrivousAppiontment'].dt.total_seconds()
 df_res['DaysSincePrivousAppiontment'] = df_res['DaysSincePrivousAppiontment'] / 60 / 60 / 24
 
+# define threshold in years and results DataFrame
 years = 10
 number_days = years*365
-
 df_res = df_res[df_res['DaysSincePrivousAppiontment'] > number_days]
 
+# export to excel
 df_res.to_excel('Days between appointments.xlsx', index= False)
-df_res
